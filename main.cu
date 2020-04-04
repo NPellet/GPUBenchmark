9e79e2fe1b5925e1616b40367d8cece54df0194e
Norman Pellet <norman.pellet@gmail.com>
2020-04-04 10:26:51 +0200

    Add parallel reduction entry in main

--- main.cu
+++ main.cu
@@ -1,10 +1,13 @@
 #include "test/test.hpp"
 
+#include "tests/parallelreduction/benchmark_parallelred.cuh"
 #include "tests/repeater/benchmark_repeater.cuh"
 #include "tests/repeater_uv/benchmark_repeater_uv.cuh"
 #include "tests/distance/benchmark_distance.cuh"
 
 int main() {
+	benchmark_parallelreduction();
+
 	benchmark_repeater();
 	benchmark_repeater_uv();
 	benchmark_distance();
